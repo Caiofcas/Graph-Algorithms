564516159733af82bbd5035c3aef490345c28460
ccastro <caiofcastro@usp.br>
2021-10-09 15:24:59 -0300

    add debugging level 2

--- EP1/main.cpp
+++ EP1/main.cpp
@@ -8,17 +8,20 @@
 
 
 void describe_digraph(Digraph dig){
-    printf("%ld %ld\n",
-        boost::num_vertices(dig), 
-        boost::num_edges(dig));
+    std::cout << boost::num_vertices(dig) << " ";
+    std::cout << boost::num_edges(dig) << std::endl;
 
     // print all edges
-    Digraph::edge_iterator edge_it, edge_it_end;
+    Digraph::edge_iterator e_it, e_it_end;
+    int n_variables = boost::num_vertices(dig)/2;
+    // std::cout << "n_variables: " << n_variables << std::endl;
 
-    for(std::tie(edge_it, edge_it_end) = boost::edges(dig);
-        edge_it != edge_it_end; edge_it++){
+    for(std::tie(e_it, e_it_end) = boost::edges(dig);
+        e_it != e_it_end; e_it++){
 
-        std::cout << (*edge_it).m_source << '\n';
+        std::cout << vertex2literal((*e_it).m_source, n_variables);
+        std::cout << " " << vertex2literal((*e_it).m_target, n_variables);
+        std::cout << std::endl;
     };
 
 }
@@ -28,9 +31,10 @@ int main(int argc, char** argv)
 {
     int d, n_variables, n_clauses;
     std::cin >> d >> n_variables >> n_clauses;
-    printf("Read d: %d\n", d);
-    printf("Read n (n_variables): %d\n", n_variables);
-    printf("Read m (n_clauses): %d\n", n_clauses);
+
+    std::cout << "Read d: " << d << std::endl;
+    std::cout << "Read n (n_variables): " <<  n_variables << std::endl;
+    std::cout << "Read m (n_clauses): " <<  n_clauses << std::endl;
     
     //read_clauses
     Digraph dig(2*n_variables); // One vertex for each literal
@@ -39,33 +43,33 @@ int main(int argc, char** argv)
     while(n_clauses--){
         int a, b; 
         std::cin >> a >> b;
-        std::cout << "Read clause: "<< a <<" "<< b << std::endl;
+        // std::cout << "Read clause: " << a << " "<< b << std::endl;
+
         boost::add_edge(
             literal2vertex(-a, n_variables),
             literal2vertex(b, n_variables),
             dig);
 
-        std::cout << "Adding edge: ";
-        std::cout << literal2vertex(-a, n_variables) << " (" << -a << ")";
-        std::cout << " <-> ";
-        std::cout << literal2vertex(b, n_variables) << " (" << b << ")";
-        std::cout << std::endl;
+        // std::cout << "Adding edge: ";
+        // std::cout << literal2vertex(-a, n_variables) << " (" << -a << ")";
+        // std::cout << " <-> ";
+        // std::cout << literal2vertex(b, n_variables) << " (" << b << ")";
+        // std::cout << std::endl;
 
         boost::add_edge(
             literal2vertex(-b, n_variables),
             literal2vertex(a, n_variables),
-            literal2vertex(-b, n_variables),
             dig);
 
-        std::cout << "Adding edge: ";
-        std::cout << literal2vertex(-b, n_variables) << " (" << -b << ")";
-        std::cout << " <-> ";
-        std::cout << literal2vertex(a, n_variables) << " (" << a << ")";
-        std::cout << std::endl;
+        // std::cout << "Adding edge: ";
+        // std::cout << literal2vertex(-b, n_variables) << " (" << -b << ")";
+        // std::cout << " <-> ";
+        // std::cout << literal2vertex(a, n_variables) << " (" << a << ")";
+        // std::cout << std::endl;
 
-        std::cout << "|V|: " << boost::num_vertices(dig);
-        std::cout << " |A|: " << boost::num_edges(dig);
-        std::cout << std::endl;
+        // std::cout << "|V|: " << boost::num_vertices(dig);
+        // std::cout << " |A|: " << boost::num_edges(dig);
+        // std::cout << std::endl;
 
     }
 
@@ -82,7 +86,6 @@ int main(int argc, char** argv)
     
     case 2:
         describe_digraph(dig);
-        // printf("Debbuging level 2 not implemented yet.\n");
         break;
     }
 }
