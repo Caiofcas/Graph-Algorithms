c70a8775c1657e3a004dc329147cea1022b85ea4
ccastro <caiofcastro@usp.br>
2021-12-13 20:03:25 -0300

    lots of fixes + debug statements

--- EP4/main.cpp
+++ EP4/main.cpp
@@ -35,10 +35,12 @@ struct DigraphBundledVertex
 
 struct DigraphBundledArc
 {
-  unsigned short capacity, flow = 0;
+  int capacity = 0, flow = 0;
   short direction = FORWARD;
-  unsigned short res_capacity = 0;
-  int orig_arc_idx;
+  int res_capacity = 0;
+  int orig_arc_idx = 0;
+  int back_arc_idx = 0;
+  int fwd_arc_idx = 0;
 };
 
 typedef boost::adjacency_list<
@@ -77,59 +79,71 @@ FlowProblem read_flow(std::istream &is)
   typename boost::graph_traits<Digraph>::vertices_size_type n;
   is >> n;
 
-  FlowProblem fd;
-  Digraph dig(n);
-  Digraph aug_dig(n);
+  FlowProblem fp;
   Vertex source, sink;
 
-  size_t m, i;
+  fp.d = Digraph(n);
+  fp.a_d = Digraph(n);
+
+  size_t m;
+  int i, j;
 
   is >> m;
   is >> source >> sink;
 
   i = 0;
+  j = 0;
   while (i < m)
   {
     int u, v;
     is >> u >> v;
     Arc a, a_f, a_b;
-    std::tie(a, std::ignore) = boost::add_edge(u-1, v-1, dig);
-    std::tie(a_f, std::ignore) = boost::add_edge(u-1, v-1, aug_dig);
-    std::tie(a_b, std::ignore) = boost::add_edge(v-1, u-1, aug_dig);
+    std::tie(a, std::ignore) = boost::add_edge(u-1, v-1, fp.d);
+    std::tie(a_f, std::ignore) = boost::add_edge(u-1, v-1, fp.a_d);
+    std::tie(a_b, std::ignore) = boost::add_edge(v-1, u-1, fp.a_d);
 
-    is >> dig[a].capacity;
+    is >> fp.d[a].capacity;
+    std::cout << "capacity: " << fp.d[a].capacity << std::endl;
 
-    aug_dig[a_f].direction = FORWARD;
-    aug_dig[a_b].direction = BACKWARD;
-    aug_dig[a_f].orig_arc_idx = aug_dig[a_b].orig_arc_idx = i;
-    fd.arc_order.push_back(a);
-    fd.aug_arc_order.push_back(a_f);
-    fd.aug_arc_order.push_back(a_b);
-    i++;
+    fp.a_d[a_f].direction = FORWARD;
+    fp.a_d[a_b].direction = BACKWARD;
+
+    fp.arc_order.push_back(a);
+    fp.a_d[a_f].orig_arc_idx = fp.a_d[a_b].orig_arc_idx = i++;
+
+    fp.aug_arc_order.push_back(a_f);
+    fp.d[a].fwd_arc_idx = j++;
+    fp.aug_arc_order.push_back(a_b);
+    fp.d[a].back_arc_idx = j++;
+
+    std::cout << "i: "<< i << " j: " << j << std::endl;
   }
 
-  fd.a_d = aug_dig;
-  fd.d = dig;
-  fd.source = source;
-  fd.sink = sink;
+  fp.source = source;
+  fp.sink = sink;
 
-  return fd;
+  return fp;
 }
 
 
-Digraph build_res_digraph(Digraph& d, Digraph& ad, std::vector<Arc>* ordering){
+Digraph build_res_digraph(Digraph* d, Digraph* ad, std::vector<Arc>* ordering){
   
   Digraph rd;
 
-  boost::copy_graph(ad, rd);
+  boost::copy_graph(*ad, rd);
   Digraph::edge_iterator e_it, e_end;
   for (tie(e_it, e_end) = boost::edges(rd); e_it!=e_end; e_it++){
     Arc orig_arc = ordering->at(rd[*e_it].orig_arc_idx);
+
+    std::cout <<"orig_arc: " << rd[*e_it].orig_arc_idx;
     int res_capacity = 0;
     if (rd[*e_it].direction == FORWARD)
-      res_capacity = d[orig_arc].capacity - d[orig_arc].flow;
+      res_capacity = (*d)[orig_arc].capacity - (*d)[orig_arc].flow;
     else
-      res_capacity = d[orig_arc].flow;
+      res_capacity = (*d)[orig_arc].flow;
+
+    std::cout <<" res_capacity: " << res_capacity << std::endl;
+    (*ad)[*e_it].res_capacity = res_capacity;
 
     if (res_capacity < 0)
       boost::remove_edge(*e_it, rd);
@@ -139,6 +153,24 @@ Digraph build_res_digraph(Digraph& d, Digraph& ad, std::vector<Arc>* ordering){
 
   return rd;
 }
+
+void print_res_digraph(
+  Digraph& d,
+  Digraph& aug_d,
+  std::vector<Arc>* orig_ordering,
+  std::vector<Arc>* aug_ordering
+){
+  for(Arc a : *orig_ordering){
+    Arc a_b, a_f;
+    std::cout << "fwd_arc_idx: " << d[a].fwd_arc_idx << std::endl;
+    std::cout << "back_arc_idx: " << d[a].back_arc_idx << std::endl;
+    a_b = aug_ordering->at(d[a].back_arc_idx);
+    a_f = aug_ordering->at(d[a].fwd_arc_idx);
+    std::cout << aug_d[a_f].res_capacity << " ";
+    std::cout << aug_d[a_b].res_capacity << std::endl;
+  }
+}
+
 /*========================================================
   ================ END DIGRAPH UTILS =====================
   ========================================================*/
@@ -226,12 +258,12 @@ void edmonds_karp(FlowProblem &fp)
   // TODO: build augmented digraph first, pass that to build_res_digraph
 
   int t = 0;
-  while (true)
+  while (true && t<1  )
   {
     // 1. Compute residual digraph of D, d_hat
-    Digraph d_f = build_res_digraph(fp.d, fp.a_d, &fp.arc_order);
+    Digraph d_f = build_res_digraph(&fp.d, &fp.a_d, &fp.arc_order);
 
-    print_res_digraph(d_f);
+    print_res_digraph(fp.d, fp.a_d, &fp.arc_order, &fp.aug_arc_order);
 //     std::tuple<
 //         bool,
 //         boost::optional<std::vector<Arc>>,
@@ -285,7 +317,7 @@ void edmonds_karp(FlowProblem &fp)
 //       std::cout << std::endl;
 //       return;
 //     }
-//     t++;
+    t++;
   }
 };
 
@@ -297,7 +329,12 @@ int main(int argc, char **argv)
 {
   FlowProblem flow_problem{read_flow(std::cin)};
 
-  // edmonds_karp(flow_problem);
+  for (auto a: flow_problem.arc_order){
+    std::cout << "arc: " << boost::source(a, flow_problem.d) +1;
+    std::cout << " -> " << boost::target(a, flow_problem.d)+1;
+    std::cout <<" (" <<  flow_problem.d[a].capacity << ")"<< std::endl;
+  }
+  edmonds_karp(flow_problem);
 
   return EXIT_SUCCESS;
 }
